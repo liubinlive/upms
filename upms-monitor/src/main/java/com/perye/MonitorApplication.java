1997b885a90a7bf509eaab846b969c7a54e97bb1
perye <11143526@qq.com>
2019-03-26 13:11:05 +0800

    dockerfile 配置

---
+++ upms-monitor/src/main/java/com/perye/MonitorApplication.java
@@ -0,0 +1,26 @@
+package com.perye;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @Author: Perye
+ * @Date: 2019-03-26
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class MonitorApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MonitorApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+
+}
